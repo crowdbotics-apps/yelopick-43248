ca63864035ebb69a7ac3049c68f89e300bbd081f
Crowdbotics <team@crowdbotics.com>
2023-09-06 08:07:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yelopick-43248",
+  url: "https://yelopick-43248.botics.co",
+  api: "https://yelopick-43248.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
